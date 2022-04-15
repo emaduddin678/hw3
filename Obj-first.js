b35566927fc0ee7db2aeedd07fe713b7a0f4fb05
emaduddin678 <emaduddin678@gmail.com>
2022-04-15 14:48:23 +0600

    first commit

---
+++ Obj-first.js
@@ -0,0 +1,47 @@
+const country = {
+    name: "Bangladesh",
+    capital: "Dhaka",
+    property: 147570,
+    totalDistrict: 64,
+    language: "Bangla"
+}
+console.log(country)
+
+
+
+const student = {}
+student.name = 'Emad';
+student.roll = 12;
+student.class = 13;
+student.department = 'CSE';
+student.cgpa = '3.21';
+student.favouriteSub = "Math";
+console.log(student)
+
+
+const car = {}
+car.name = "Range Rover";
+car.model = 737;
+car.weight = "1150kg";
+car.color = "Black";
+car.maxSpeed = "335km/h";
+console.log(car)
+
+
+const person = {
+    firstName: "Emad",
+    lastName: "Uddin",
+    subject: "Mern Stack",
+    id: 1462,
+    learnFrom: "School of Skill" 
+}
+console.log(person)
+
+
+const pc = {}
+pc.processor = "core i-5 6402p";
+pc.hdd = "1 Tb";
+pc.ssd = "256 gb";
+pc.motherboard = "msi";
+pc.ram = "2133 Hz";
+console.log(pc)
